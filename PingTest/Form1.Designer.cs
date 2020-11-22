0e6289c446b4e3c40def2f579594b8c422f9402a
Daniel Werf <danielw@bytesnap.co.uk>
2020-11-22 20:03:30 +0000

    first commit

---
+++ PingTest/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace PingTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblPingVal = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblPingValAvg = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Address or Hostname:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(177, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(438, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "8.8.8.8";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDown2, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.lblPingValAvg, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDown4, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblPingVal, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDown1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDown3, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(618, 381);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // lblPingVal
+            // 
+            this.lblPingVal.AutoSize = true;
+            this.lblPingVal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPingVal.Location = new System.Drawing.Point(177, 54);
+            this.lblPingVal.Name = "lblPingVal";
+            this.lblPingVal.Size = new System.Drawing.Size(56, 29);
+            this.lblPingVal.TabIndex = 3;
+            this.lblPingVal.Text = "N/A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ping:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "PingRate (ms):";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.numericUpDown1.Location = new System.Drawing.Point(177, 111);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(438, 26);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label5.Location = new System.Drawing.Point(3, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Ok:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Green;
+            this.label6.Location = new System.Drawing.Point(3, 324);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 20);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Good:";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.numericUpDown3.Location = new System.Drawing.Point(177, 273);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(438, 26);
+            this.numericUpDown3.TabIndex = 4;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.NumericUpDown3_ValueChanged);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.numericUpDown4.Location = new System.Drawing.Point(177, 327);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(438, 26);
+            this.numericUpDown4.TabIndex = 5;
+            this.numericUpDown4.ValueChanged += new System.EventHandler(this.NumericUpDown4_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ping Avg.:";
+            // 
+            // lblPingValAvg
+            // 
+            this.lblPingValAvg.AutoSize = true;
+            this.lblPingValAvg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPingValAvg.Location = new System.Drawing.Point(177, 162);
+            this.lblPingValAvg.Name = "lblPingValAvg";
+            this.lblPingValAvg.Size = new System.Drawing.Size(56, 29);
+            this.lblPingValAvg.TabIndex = 7;
+            this.lblPingValAvg.Text = "N/A";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 216);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Avg. Samples:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.numericUpDown2.Location = new System.Drawing.Point(177, 219);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(438, 26);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.NumericUpDown2_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 381);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Ping Test";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblPingVal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblPingValAvg;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PingTest/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace PingTest
+{
+    public partial class Form1 : Form
+    {
+        private List<int> averagePing = new List<int>();
+
+        public int PingInterval
+        {
+            get
+            {
+                return pingInterval;
+            }
+            set
+            {
+                pingInterval = value;
+            }
+        }
+        private int pingInterval = 250;
+
+        private Thread pingThread;
+        private bool pingThreadRunning = false;
+
+        public int AverageSamples
+        {
+            get
+            {
+                return averageSamples;
+            }
+            set
+            {
+                averageSamples = value;
+            }
+        }
+        private int averageSamples = 5;
+
+        public int OkPing
+        {
+            get
+            {
+                return okPing;
+            }
+            set
+            {
+                okPing = value;
+            }
+        }
+        private int okPing = 50;
+        public int GoodPing
+        {
+            get
+            {
+                return goodPing;
+            }
+            set
+            {
+                goodPing = value;
+            }
+        }
+        private int goodPing = 25;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = PingInterval;
+            numericUpDown2.Value = AverageSamples;
+            numericUpDown3.Value = OkPing;
+            numericUpDown4.Value = GoodPing;
+
+            pingThreadRunning = true;
+            pingThread = new Thread(new ThreadStart(PingThread));
+            pingThread.IsBackground = true;
+            pingThread.Start();           
+        }
+
+        public void PingThread()
+        {
+            while (pingThreadRunning)
+            {
+                
+                Thread.Sleep(pingInterval);
+
+                Ping pinger = new Ping();
+                PingReply pingReply;
+
+                long ping = 0;
+
+                try
+                {
+                    IPHostEntry iPHostEntry = Dns.GetHostEntry(textBox1.Text);
+                    pingReply = pinger.Send(iPHostEntry.AddressList[0], pingInterval / 2);
+                    ping = pingReply.RoundtripTime;
+
+                    while (averagePing.Count > averageSamples)
+                        averagePing.RemoveAt(0);
+
+                    averagePing.Add((int)ping);
+
+                    setUiLabel(lblPingVal, ping);
+                    setUiLabel(lblPingValAvg, averagePing.Average());
+                }
+                catch
+                {
+                    ping = -1;
+                }
+
+                
+            }
+        }
+
+        private void setUiLabel(Label ui, double ping)
+        {
+            this.Invoke((MethodInvoker)delegate ()
+            {
+                try
+                {
+                    if (ping > 0)
+                        ui.Text = ping.ToString("0");
+                    else
+                        ui.Text = "N/A";
+
+                    if (ping <= 0)
+                        ui.ForeColor = Color.Red;
+                    else if (ping > goodPing)
+                        ui.ForeColor = Color.DarkOrange;
+                    else if (ping > okPing)
+                        ui.ForeColor = Color.Red;
+                    else
+                        ui.ForeColor = Color.Green;
+                }
+                catch
+                {
+                    // form is closed
+                }
+            });
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            pingThreadRunning = false;
+        }
+
+        private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            PingInterval = (int)numericUpDown1.Value;
+        }
+        
+        private void NumericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            AverageSamples = (int)numericUpDown2.Value;
+        }
+        
+        private void NumericUpDown3_ValueChanged(object sender, EventArgs e)
+        {            
+            OkPing = (int)numericUpDown3.Value;
+            checkPing();
+        }
+
+        private void NumericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+            GoodPing = (int)numericUpDown4.Value;
+            checkPing();
+        }
+
+        private void checkPing()
+        {
+            if (okPing <= goodPing)
+            {
+                if (goodPing >= 100)
+                {
+                    goodPing = okPing - 1;
+                    numericUpDown4.Value = goodPing;
+                }
+                else
+                {
+                    okPing = goodPing + 1;
+                    numericUpDown3.Value = okPing;
+                }
+            }
+        }
+    }
+}
